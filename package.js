857920629260bc4dc94152536dd74e1d8af0cb2a
Nick Dodson <silentcicero@outlook.com>
2015-07-03 17:55:52 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
-  summary: "Create, read and edit .zip files on server",
+  summary: "Create, read and edit .zip files on the client",
   version: "2.4.0_1",
-  git: "https://github.com/udondan/meteor-jszip.git",
+  git: "https://github.com/silentcicero/meteor-jszip.git",
   environments: 'server'
 });
 
@@ -11,6 +11,6 @@ Npm.depends({
 
 Package.on_use(function (api) {
   api.versionsFrom("METEOR@0.9.0");
-  api.add_files('lib/saveas.js', 'server');
-  api.export(['JSZip'], 'server');
+  api.add_files('lib/saveas.js', 'client');
+  api.export(['JSZip'], 'client');
 });
